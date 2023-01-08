fb5a90681dba7c52ad536e35d1900ad20868669d
Samhan097 <48414384+Samhan097@users.noreply.github.com>
2023-01-08 11:12:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import turtle
+
+Qazi_turtle = turtle.Turtle()
+Qazi_turtle.speed(34)
+def square():
+
+    Qazi_turtle.forward(100)
+    Qazi_turtle.right(90)
+    Qazi_turtle.forward(100)
+    Qazi_turtle.right(90)
+    Qazi_turtle.forward(100)
+    Qazi_turtle.right(90)
+    Qazi_turtle.forward(100)
+# square()
+# Qazi_turtle.forward(200)
+# square()
+
+# elephant_weight = 3000
+# ant_weight = 0.1
+
+# if elephant_weight < ant_weight:
+#     square()
+# else:
+#     Qazi_turtle.forward(300)
+
+for count in range(4):
+    square()
